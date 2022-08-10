81ece2afaad3c4e3a8b6acbeac328cd8070d956a
joelmukanya <joel@lifechoices.co.za>
2022-08-10 10:37:39 +0200

    Updates

---
+++ src/components/InStoreComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <button type="button" class="btn btn-success">
+        <span class="badge text-bg-secondary">Available</span>
+    </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/OurProductsView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row d-flex ourProducts">
+        <div class="card" v-for="(prod, index) in products"
+            :key="index">
+            
+            <h3 class="display-3">
+                <span>Category</span>
+                {{prod.category}}
+            </h3>
+            <img :src="prod.imgURL" class="card-img-top img-fluid" 
+                :alt="prod.title">
+            <div class="card-body">
+                <h5 class="card-title">{{prod.title}}</h5>
+                <p class="lead">
+                    <span>Spec</span>
+                    {{prod.descripton}}
+                </p>
+                <ul class="navbar-nav">
+                    <li>
+                        Price: R<span>{{}}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>            
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
