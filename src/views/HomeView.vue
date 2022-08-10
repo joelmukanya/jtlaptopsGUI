1b0aab2844d4f1b7ed7cfe918574880ace676cb4
joelmukanya <joel@lifechoices.co.za>
2022-08-10 11:04:55 +0200

    Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,20 @@
 <template>
   <div class="container-fluid">
-    <SpinnerComponent v-show="showSpinner"/>
     <iframe :src="getRoadMap" class="container vh-100"
      frameborder="0" ></iframe>
   </div>
 </template>
 <script>
-import SpinnerComponent from '@/components/SpinnerComponent.vue'
+
 // @ is an alias to /src
 
 export default {
   computed: {
     getRoadMap() {
         return this.$store.state.readMeData
-      },
-    showSpinner() {
-      return this.$store.state.showSpinner
     }
   },
-  components: {
-    SpinnerComponent
-  },
+  components: {  },
   mounted() {
     this.$store.dispatch('fetchReadMeUrl');
   }
